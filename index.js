4129ee10c1bb173b93b6ded4ee3a06f973e7514f
John Daniel <jnoromba@uci.edu>
2024-04-25 13:29:29 -0700

    supabase init ii

---
+++ index.js
@@ -0,0 +1,9 @@
+// Project setup derived from supabase examples
+// https://github.com/supabase/examples-archive/tree/main/supabase-js-v1/auth/javascript-auth
+
+const supabase = createClient(
+  "https://rpnukhbkfykwutyntnkw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwbnVraGJrZnlrd3V0eW50bmt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwNzU0MzUsImV4cCI6MjAyOTY1MTQzNX0.c9PQXsg2ADUdpNtyY8_eLmKDpfSr11W4jXdHp0BBQG4",
+);
+
+console.log("hello world!");
