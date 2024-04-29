a5f4fd039d2b0ecaa9126797edaf11c4e3f826d6
John Daniel <jnoromba@uci.edu>
2024-04-29 16:39:19 -0700

    send values over

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const create = document.getElementById("createaccount");
 
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
 create.addEventListener("click", async () => {
-  const email = document.getElementById("email");
-  const password = document.getElementById("password");
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
   const { data, error } = await supabase.auth.signUp({
     email: email,
