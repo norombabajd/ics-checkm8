fb342560658cc0db4bd60eff0bc6180f8c94672c
John Daniel <jnoromba@uci.edu>
2024-04-29 16:25:52 -0700

    login

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Project setup derived from supabase examples
 // https://github.com/supabase/examples-archive/tree/main/supabase-js-v1/auth/javascript-auth
+// https://supabase.com/docs/reference/javascript/installing
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabase = createClient(
@@ -7,4 +8,10 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwbnVraGJrZnlrd3V0eW50bmt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwNzU0MzUsImV4cCI6MjAyOTY1MTQzNX0.c9PQXsg2ADUdpNtyY8_eLmKDpfSr11W4jXdHp0BBQG4",
 );
 
-console.log("hello world!");
+const login = document.getElementById("login");
+const create = document.getElementById("createaccount");
+
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+create.addEventListener("click", async () => {
+  console.log(alert("AHHHH"));
+});
