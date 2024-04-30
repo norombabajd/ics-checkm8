e089aed2a9dd9eece8686e1339997831a5f59040
John Daniel <jnoromba@uci.edu>
2024-04-30 14:09:51 -0700

    name change for oauth

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwbnVraGJrZnlrd3V0eW50bmt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwNzU0MzUsImV4cCI6MjAyOTY1MTQzNX0.c9PQXsg2ADUdpNtyY8_eLmKDpfSr11W4jXdHp0BBQG4",
 );
 
-const create = document.getElementById("google-oauth");
-
+const create = document.getElementById("createaccount");
 
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
 create.addEventListener("click", async () => {
