347435959e07d306469ef85dcd50bfdf0e7fd7d2
John Daniel <jnoromba@uci.edu>
2024-04-30 13:42:22 -0700

    supabase oauth google

--- index.js
+++ index.js
@@ -3,21 +3,24 @@
 // https://supabase.com/docs/reference/javascript/installing
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
+
 const supabase = createClient(
   "https://rpnukhbkfykwutyntnkw.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwbnVraGJrZnlrd3V0eW50bmt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwNzU0MzUsImV4cCI6MjAyOTY1MTQzNX0.c9PQXsg2ADUdpNtyY8_eLmKDpfSr11W4jXdHp0BBQG4",
 );
 
-const login = document.getElementById("login");
-const create = document.getElementById("createaccount");
+const create = document.getElementById("google-oauth");
+
 
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
 create.addEventListener("click", async () => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google'
+  })
 
-  const { data, error } = await supabase.auth.signUp({
-    email: email,
-    password: password,
-  });
+  console.log(data);
+
+  supabase.auth.signOut();
+
 });
