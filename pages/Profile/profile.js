143c1694c279795c3dc1aa7a2acb147226c4ba4e
Kevin <107275055+KevinJli23@users.noreply.github.com>
2024-05-02 23:50:13 -0700

    -- added attendence page and search page
    - re linked and added buttons to navigate
    - created menu page

--- pages/Profile/profile.js
+++ pages/Profile/profile.js
@@ -1,4 +1,23 @@
-import { supabase } from '../../index.js';
+// import { supabase } from '../../index.js';
 
-const { data: { user } } = await supabase.auth.getUser()
-console.log(data);
+// const { data: { user } } = await supabase.auth.getUser()
+// console.log(data);
+
+
+
+
+// Kevin  commented out the above because it prevented the functions to run
+window.addEventListener('DOMContentLoaded', () => {
+    showSubMenu('general');
+  });
+function showSubMenu(submenuId) {
+        // Hide all submenus
+        const submenus = document.querySelectorAll('.submenu');
+        submenus.forEach(submenu => {
+            submenu.classList.remove('active');
+        });
+
+        // Show the selected submenu
+        const selectedSubmenu = document.getElementById(`submenu-${submenuId}`);
+        selectedSubmenu.classList.add('active');
+    }
