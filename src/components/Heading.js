c501877720e834cfb98abf1ac92b2ab6ed70567e
John Daniel <jnoromba@uci.edu>
2024-05-07 16:28:25 -0700

    added tailwindcss + header component

---
+++ src/components/Heading.js
@@ -0,0 +1,21 @@
+function Header() {
+    return <header>
+    <div id="navbar" class="flex justify-between p-5 bg-[#BBDEF0]">
+      <div>
+        <b className="text-lg">checkm8</b>
+      </div>
+      <div>
+        {/* to do: routing */}
+        <nav className="space-x-4 content-evenly">
+        <button id="footer-button">Dashboard</button>
+            <button id="footer-button">Check-in</button>
+            <button id="footer-button">Menu</button>
+            <button id="footer-button">Profile</button>
+        </nav>
+      </div>
+
+    </div>
+    </header>
+  }
+
+  export default Header;
